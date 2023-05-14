21e2496df1bd6289e3374c357a0207de1382e69f
John A Stevenson <johnalexanderstevenson@gmail.com>
2023-05-14 16:59:37 +0100

    Make full screen with more addresses

--- docs/data/locations_1.js
+++ docs/data/locations_1.js
@@ -3,6 +3,7 @@ var json_locations_1 = {
 "name": "locations_1",
 "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
 "features": [
-{ "type": "Feature", "properties": { "address": "School" }, "geometry": { "type": "Point", "coordinates": [ -3.14869464, 55.88157298 ] } }
+{ "type": "Feature", "properties": { "address": "School" }, "geometry": { "type": "Point", "coordinates": [ -3.14869464, 55.88157298 ] } },
+{ "type": "Feature", "properties": { "address": "27 Polton Road" }, "geometry": { "type": "Point", "coordinates": [ -3.14496008, 55.8754451 ] } }
 ]
 }
