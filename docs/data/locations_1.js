d78ccd92f2fb6257fa23633f3d8ccd8e3f580ebe
John A Stevenson <johnalexanderstevenson@gmail.com>
2023-05-14 16:34:16 +0100

    Add first draft web map

---
+++ docs/data/locations_1.js
@@ -0,0 +1,8 @@
+var json_locations_1 = {
+"type": "FeatureCollection",
+"name": "locations_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "address": "School" }, "geometry": { "type": "Point", "coordinates": [ -3.14869464, 55.88157298 ] } }
+]
+}
